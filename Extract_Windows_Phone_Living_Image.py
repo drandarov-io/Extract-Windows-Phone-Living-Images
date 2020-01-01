95144100a70d8a3c93caf23f0cd404703a6cc3d3
Dmitrij Drandarov <dmitrij.drandarov@outlook.com>
2020-01-01 03:38:39 +0100

    Fix behaviour for dng files and photos without living image

--- Extract_Windows_Phone_Living_Image.py
+++ Extract_Windows_Phone_Living_Image.py
@@ -3,7 +3,7 @@ import shutil
 import zipfile
 
 for filename in os.listdir(os.curdir):
-    if filename.startswith('WP_'):
+    if filename.startswith('WP_') and filename.endswith('.jpg') and zipfile.is_zipfile(filename):
         print(filename)
         with zipfile.ZipFile(filename) as z:
             with z.open('formats/living/living.mp4') as zf, open(filename.replace('.jpg', '') + '_motion.mp4', 'wb') as f:
